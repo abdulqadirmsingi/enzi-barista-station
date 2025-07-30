4441344c69455a573745b4ff65295f6212cc8cba
Abdulqadir Mohammed <qadirmsingi007@gmail.com>
2025-07-30 20:34:46 +0300

    fix: api deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,7 @@ import {
 } from "@/types";
 
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://enzi-backend.onrender.com/api";
 
 class ApiService {
   private async request<T>(
